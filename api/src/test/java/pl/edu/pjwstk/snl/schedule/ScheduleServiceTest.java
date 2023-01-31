6c61765a632617dcdb1276a57db8490881a66124
Maciej Milewski <macias.argedius@gmail.com>
2023-01-31 22:48:03 +0100

    Added ScheduleServiceTest

--- api/src/test/java/pl/edu/pjwstk/snl/schedule/ScheduleServiceTest.java
+++ api/src/test/java/pl/edu/pjwstk/snl/schedule/ScheduleServiceTest.java
@@ -1,31 +1,70 @@
 package pl.edu.pjwstk.snl.schedule;
 
+import io.micrometer.core.instrument.internal.Mergeable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import pl.edu.pjwstk.snl.team.Team;
+import pl.edu.pjwstk.snl.team.TeamService;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@DirtiesContext(classMode= DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ScheduleServiceTest {
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @Autowired
+    private TeamService teamService;
 
     @Test
     void findAll() {
+        List<Schedule> schedules = scheduleService.findAll();
+
+        Assertions.assertTrue(schedules.size() == 2);
     }
 
     @Test
     void findById() {
+        Assertions.assertTrue(
+                scheduleService.findById(1L)
+                        .orElse(null)
+                        .getDescription()
+                        .equals("Plan spotkań na 2023")
+        );
     }
 
     @Test
     void findByTeamId() {
+        Assertions.assertTrue(
+                scheduleService.findByTeamId(1L)
+                        .orElse(null)
+                        .getDescription()
+                        .equals("Plan spotkań na 2023")
+        );
     }
 
     @Test
     void save() {
+        Assertions.assertTrue(scheduleService.existsById(1L));
+        Assertions.assertTrue(scheduleService.existsById(1L));
     }
 
     @Test
     void deleteById() {
+        scheduleService.deleteById(1L);
+        Assertions.assertFalse(scheduleService.existsById(1L));
     }
 
     @Test
     void existsById() {
+        Assertions.assertTrue(scheduleService.existsById(1L));
     }
 }
